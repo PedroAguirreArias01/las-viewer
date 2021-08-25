9a727e60740fa2f472b15072eb884e5c263d92e4
Pedro Aguirre <pedroaguirrearias01@gmail.com>
2021-08-24 20:17:17 -0500

    Se agregan diagramas

---
+++ las-view/src/main/java/com/lasviewer/lasview/LasViewApplication.java
@@ -0,0 +1,13 @@
+package com.lasviewer.lasview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LasViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LasViewApplication.class, args);
+	}
+
+}
---
+++ las-view/src/test/java/com/lasviewer/lasview/LasViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasviewer.lasview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LasViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
