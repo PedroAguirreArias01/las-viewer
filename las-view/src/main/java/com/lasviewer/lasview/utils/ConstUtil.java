2478ad8703cd148a6495a46c687ba4538b238256
julian <julian_ri18@hotmail.com>
2022-03-08 19:05:13 -0500

    Ajuste Constantes

--- las-view/src/main/java/com/lasviewer/lasview/utils/ConstUtil.java
+++ las-view/src/main/java/com/lasviewer/lasview/utils/ConstUtil.java
@@ -9,4 +9,30 @@ public class ConstUtil {
 	public static final String PARAMETER_INFO = "~P";
 	public static final String OTHER_INFO ="~O";
 	public static final String VALUE_NULL = "-999.2500";
+	
+	// DATA WELL_INFO AND LOG_WELL
+	public static final String START_WELL = "STRT";
+	public static final String STOP_WELL = "STOP";
+	public static final String STEP_WELL = "STEP";
+	public static final String LOG_DATE_WELL = "DATE";
+	
+	
+	public static final String ID_WELL = "UWI";
+	public static final String NAME_WELL = "WELL";
+	public static final String COMPANY_WELL = "COMP";
+	
+	
+	// LOCATION_WELL
+	public static final String LOCATION_WELL = "LOC";
+	public static final String FIELD_WELL = "FLD";
+	public static final String STATE_WELL = "STAT";
+	public static final String SECTION_WELL = "SECT";
+	public static final String TOWNSHIP_WELL = "TOWN";
+	public static final String COUNTRY_WELL = "CNTY";
+	public static final String RANGE_WELL = "RANG";
+	
+	
+	// CURVE_INFORMATION_PARAM
+	//public static final String RANGE_WELL = "RANG";
+	
 }
